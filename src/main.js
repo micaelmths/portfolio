8bc10818deadeb2b778ec53da0c1830e3af94253
Micael Matheus <micael.matheus@live.com>
2021-08-28 16:22:05 -0300

    add vuetify

--- src/main.js
+++ src/main.js
@@ -4,10 +4,12 @@ import router from './router'
 import VueTilt from 'vue-tilt.js'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import vuetify from './plugins/vuetify'
 
 Vue.config.productionTip = false
 Vue.use(VueTilt)
 new Vue({
   router,
+  vuetify,
   render: h => h(App)
 }).$mount('#app')
