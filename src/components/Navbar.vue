96ad2b4a66b24b4796302bc2b27dab6d935ecb63
Micael Matheus <micael.matheus@live.com>
2021-08-30 18:49:24 -0300

    add menu do navbar para dispositivos menores

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="navbar-container d-flex align-items-center" :class="isScrolling">
-    <div class="d-flex align-items-center col-12">
+    <div class="navbar d-flex align-items-center col-12">
       <a href="#home">
         <h1 class="col-2">Micael</h1>
       </a>
@@ -10,11 +10,35 @@
           <li><a href="#sobre">Sobre</a></li>
           <li><a href="#habilidades">Habilidades</a></li>
           <li><a href="#resumo">Resumo</a></li>
-          <li><a href="#portifolio">Portifólio</a></li>
           <li><a href="#contato">Contato</a></li>
           <li><a target="_blank" href="https://micaelmths.github.io/meusite/">Blog</a></li>
         </ul>
       </nav>
+      <template>
+        <div class="btn-menu text-center" data-app>
+          <v-menu offset-y dark transition="true">
+            <template v-slot:activator="{ on, attrs }">
+              <v-btn
+              color="#2a9d8f"
+                elevation="2"
+                v-bind="attrs"
+                v-on="on"
+              >
+                <v-icon color="#fff">fas fa-bars</v-icon>
+              </v-btn>
+            </template>
+            <v-list>
+              <v-list-item
+                v-for="(item, index) in items"
+                :key="index"
+                :href="item.href"
+              >
+                <v-list-item-title>{{ item.title }}</v-list-item-title>
+              </v-list-item>
+            </v-list>
+          </v-menu>
+        </div>
+      </template>
     </div>
   </div>
 </template>
@@ -23,7 +47,26 @@
 export default {
   data () {
     return {
-      scrollY: null
+      screenWidth: null,
+      scrollY: null,
+      items: [
+        {
+          title: 'Sobre',
+          href: '#sobre'
+        },
+        {
+          title: 'Habilidades',
+          href: '#habilidades'
+        },
+        {
+          title: 'Contato',
+          href: '#contato'
+        },
+        {
+          title: 'Blog',
+          href: 'https://micaelmths.github.io/meusite/'
+        }
+      ]
     }
   },
   computed: {
@@ -43,12 +86,18 @@ export default {
 .navbar-container {
   width: 100%;
   height: 70px;
+  justify-content: space-between;
   background: var(--bg-color);
   position: fixed;
   left: 0;
-  padding: 0 200px;
   z-index: 10;
   transition: box-shadow 0.3s;
+}
+.navbar {
+  padding: 0 50px;
+}
+.btn-menu {
+  display: none;
 }
 .isScrolling {
   box-shadow: -1px 8px 9px -1px rgba(0, 0, 0, 0.25);
@@ -70,4 +119,20 @@ li {
 a:hover {
   color: var(--primary-color);
 }
+
+@media (min-width: 1400px) {
+  .navbar {
+    padding: 0 200px;
+  }
+}
+
+@media (max-width: 850px) {
+  nav {
+    display: none;
+  }
+  .btn-menu {
+    display: flex;
+  }
+}
+
 </style>
